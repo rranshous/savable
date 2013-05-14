c2dc133cd19a722201f34d1a6d10173e0b75d480
Robby Ranshous <rranshous@gmail.com>
2013-05-14 15:08:00 -0400

    updated version keeper + spec

---
+++ spec/savable/version_keeper_spec.rb
@@ -0,0 +1,73 @@
+require_relative '../../app.rb'
+
+describe Savable::VersionKeeper do
+  
+  let(:version_keeper) { Class.new.new.extend(Savable::VersionKeeper) }
+
+  describe "#versioned_name" do
+
+    it "has accessor" do
+      expect(version_keeper).to respond_to :versioned_name
+    end
+
+    it "does not have setter" do
+      expect(version_keeper).not_to respond_to :versioned_name=
+    end
+
+    it "raises error if you don't provide a name" do
+      expect{ version_keeper.versioned_name }.to raise_error
+    end
+
+    it "raises error if you don't provide current version" do
+      expect{ version_keeper.versioned_name }.to raise_error
+    end
+
+    it "succeeds if it has a version and a name" do
+      def version_keeper.name
+        'testname'
+      end
+      def version_keeper.current_version
+        'testversion'
+      end
+      expect{ version_keeper.versioned_name }.not_to raise_error
+    end
+
+    it "bases the versioned name off the name" do
+      def version_keeper.name
+        'testname'
+      end
+      def version_keeper.current_version
+        'testversion'
+      end
+      expect(version_keeper.versioned_name['testname']).not_to eq nil
+    end
+
+    it "includes current_version in versioned name" do
+      def version_keeper.name
+        'testname'
+      end
+      def version_keeper.current_version
+        'test_version'
+      end
+      versioned_name = version_keeper.versioned_name
+      expect(versioned_name['test_version']).not_to eq nil
+    end
+  end
+
+  describe "#current_version" do
+
+    it "provides getter for current version" do
+      expect(version_keeper).to respond_to :current_version
+    end
+
+    it "provides setter for current version" do
+      expect(version_keeper).to respond_to :current_version=
+    end
+
+    it "keeps track of it's current version" do
+      version_keeper.current_version = :testversion
+      expect(version_keeper.current_version).to eq :testversion
+    end
+  end
+
+end
--- version_keeper.rb
+++ version_keeper.rb
@@ -1,9 +1,19 @@
 module Savable
-
   module VersionKeeper
 
-    
+    def versioned_name
+      raise "Must provide name" if !respond_to?(:name) || name.nil?
+      raise "Must set version first" if current_version.nil?
+      "#{name}.T#{current_version}"
+    end
+
+    def current_version
+      @current_version
+    end
+
+    def current_version= our_version
+      @current_version = our_version
+    end
 
   end
-
 end
