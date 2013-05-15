d49854df33b38498189305c79af3bc2236ad7ad8
Robby Ranshous <rranshous@gmail.com>
2013-05-14 23:17:14 -0400

    moved Master from spec to app and renamed Savable

--- app.rb
+++ app.rb
@@ -6,3 +6,86 @@ require_relative './meta_keeper.rb'
 require_relative './disk_backer_meta.rb'
 require_relative './disk_accessor.rb'
 require_relative './version_keeper.rb'
+
+class Savable::Savable < Savable::Base
+  include Savable::NamedBacker
+  include Savable::DiskBacker
+  include Savable::DiskBacker::Serializor
+  include Savable::MetaKeeper
+  include Savable::DiskBacker::Meta
+  include DiskAccessor
+
+  def save
+    disk_save
+    disk_meta_save
+  end
+
+  def load
+    disk_load
+    disk_meta_load
+  end
+
+  def root_save_path= new_root
+    self.disk_save_root_path = new_root
+    self.disk_meta_save_root_path = new_root
+  end
+
+  def name
+    file_name
+  end
+
+end
+
+class Savable::SavableVersioned < Savable::Savable
+
+  include Savable::VersionKeeper
+
+  def disk_save_name
+    versioned_name
+  end
+
+  def disk_meta_save_name
+    versioned_name
+  end
+
+  def save
+    self.current_version = Time.now.to_f.to_s
+    super # save down data
+    create_symbolic_link_to_blob
+    create_symbolic_link_to_meta
+  end
+
+  def create_symbolic_link_to_blob
+    # HACK
+    link_path = self.disk_save_path.gsub /.T.*?T/, '' # remove version
+    File.unlink link_path if File.exist? link_path
+    FileUtils.symlink disk_save_path, link_path
+  end
+
+  def create_symbolic_link_to_meta
+    link_path = self.disk_meta_save_path.gsub /T.*?T/, '' # remove version
+    File.unlink link_path if File.exist? link_path
+    FileUtils.symlink disk_meta_save_path, link_path
+  end
+
+  def load
+    if current_version.nil?
+      # we need to figure out what the last version was
+      latest = find_latest_version
+      raise "No previous version found" if latest.nil?
+      self.current_version = latest
+    end
+    super
+  end
+
+  private
+
+  def find_latest_version
+    Dir.glob("#{disk_meta_save_root_path}/*.#{meta_file_extension}")
+    .map { |path| get_version_from_file_name File.basename path }
+    .compact
+    .sort_by { |v| v.to_f }
+    .last
+  end
+
+end
--- spec/savable_spec.rb
+++ spec/savable_spec.rb
@@ -1,89 +1,7 @@
 require_relative '../app.rb'
 
-class Master < Savable::Base
-  include Savable::NamedBacker
-  include Savable::DiskBacker
-  include Savable::DiskBacker::Serializor
-  include Savable::MetaKeeper
-  include Savable::DiskBacker::Meta
-  include DiskAccessor
 
-  def save
-    disk_save
-    disk_meta_save
-  end
-
-  def load
-    disk_load
-    disk_meta_load
-  end
-
-  def root_save_path= new_root
-    self.disk_save_root_path = new_root
-    self.disk_meta_save_root_path = new_root
-  end
-
-  def name
-    file_name
-  end
-
-end
-
-class MasterVersioned < Master
-
-  include Savable::VersionKeeper
-
-  def disk_save_name
-    versioned_name
-  end
-
-  def disk_meta_save_name
-    versioned_name
-  end
-
-  def save
-    self.current_version = Time.now.to_f.to_s
-    super # save down data
-    create_symbolic_link_to_blob
-    create_symbolic_link_to_meta
-  end
-
-  def create_symbolic_link_to_blob
-    # HACK
-    link_path = self.disk_save_path.gsub /.T.*?T/, '' # remove version
-    File.unlink link_path if File.exist? link_path
-    FileUtils.symlink disk_save_path, link_path
-  end
-
-  def create_symbolic_link_to_meta
-    link_path = self.disk_meta_save_path.gsub /T.*?T/, '' # remove version
-    File.unlink link_path if File.exist? link_path
-    FileUtils.symlink disk_meta_save_path, link_path
-  end
-
-  def load
-    if current_version.nil?
-      # we need to figure out what the last version was
-      latest = find_latest_version
-      raise "No previous version found" if latest.nil?
-      self.current_version = latest
-    end
-    super
-  end
-
-  private
-
-  def find_latest_version
-    Dir.glob("#{disk_meta_save_root_path}/*.#{meta_file_extension}")
-    .map { |path| get_version_from_file_name File.basename path }
-    .compact
-    .sort_by { |v| v.to_f }
-    .last
-  end
-
-end
-
-describe Savable do
+describe Savable::Savable do
 
   before(:each) do
     `rm -rf /tmp/_test`
@@ -91,14 +9,14 @@ describe Savable do
   end
 
   it "does simple blob saves / loads" do
-    s = Master.new
+    s = Savable::Savable.new
     s.file_name = 'testfile'
     s.data = 'test content'
     s.root_save_path = '/tmp/_test/'
     s.save
     expect(File.exist?('/tmp/_test/testfile.blob')).to eq true
     expect(File.open('/tmp/_test/testfile.blob').read).to eq 'test content'
-    s2 = Master.new
+    s2 = Savable::Savable.new
     s2.file_name = 'testfile'
     s2.root_save_path = '/tmp/_test/'
     s2.load
@@ -106,12 +24,12 @@ describe Savable do
   end
 
   it "does meta data saves / loads" do
-    s = Master.new
+    s = Savable::Savable.new
     s.root_save_path = '/tmp/_test/'
     s.file_name = 'testfile2'
     s['success'] = :true
     s.save
-    s2 = Master.new
+    s2 = Savable::Savable.new
     s2.root_save_path = '/tmp/_test/'
     s2.file_name = 'testfile2'
     s2.load
@@ -119,7 +37,7 @@ describe Savable do
   end
 
   it "updates versions" do
-    s = MasterVersioned.new
+    s = Savable::SavableVersioned.new
     s.root_save_path = '/tmp/_test/'
     s.file_name = 'testfile3'
     s.data = 'testdata'
@@ -132,12 +50,12 @@ describe Savable do
   end
 
   it "loads previous versions data" do
-    s = MasterVersioned.new
+    s = Savable::SavableVersioned.new
     s.root_save_path = '/tmp/_test/'
     s.file_name = 'testfile3'
     s.data = 'testdata'
     s.save
-    s2 = MasterVersioned.new
+    s2 = Savable::SavableVersioned.new
     s2.root_save_path = '/tmp/_test/'
     s2.file_name = 'testfile3'
     s2.load
@@ -145,12 +63,12 @@ describe Savable do
   end
 
   it "updates it's current version after load" do
-    s = MasterVersioned.new
+    s = Savable::SavableVersioned.new
     s.root_save_path = '/tmp/_test/'
     s.file_name = 'testfile3'
     s.data = 'testdata'
     s.save
-    s2 = MasterVersioned.new
+    s2 = Savable::SavableVersioned.new
     s2.root_save_path = '/tmp/_test/'
     s2.file_name = 'testfile3'
     s2.load
@@ -158,7 +76,7 @@ describe Savable do
   end
 
   it "keeps a symbolic link to the newest version" do
-    s = MasterVersioned.new
+    s = Savable::SavableVersioned.new
     s.root_save_path = '/tmp/_test/'
     s.file_name = 'testfile3'
     s.data = 'testdata'
@@ -166,7 +84,7 @@ describe Savable do
     symbolic_path = '/tmp/_test/testfile3.blob'
     expect(File.exist? symbolic_path).to eq true
     expect(File.open(symbolic_path).read).to eq 'testdata'
-    s2 = MasterVersioned.new
+    s2 = Savable::SavableVersioned.new
     s2.root_save_path = '/tmp/_test/'
     s2.file_name = 'testfile3'
     s2.data = 'testdata2'
